93f1e36cd15cde01044ddde2e09a003609f8a529
石塚 <Aoee6752@gmail.com>
2018-05-18 10:39:20 +0900

    月曜日と火曜日

---
+++ テスト.php
@@ -0,0 +1,15 @@
+<?php
+$a = 128;
+$b = 6;
+
+$c = $a + $b;
+echo "a+b=$c<br>/n";
+
+$c = $a - $b;
+echo "a-b=$c<br>/n";
+    
+  $c = $a * $b;
+echo "a×b=$c<br>/n";
+    
+    $c = $a / $b;
+echo "a/b=$c<br/n";
